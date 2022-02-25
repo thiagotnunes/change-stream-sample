599386b2baaeea72dbe35cf0f8d9f2665a515ff6
Thiago Nunes <thiagotnunes@google.com>
2022-02-25 11:07:07 +1100

    fix: print out string records to gcs
    Instead of commit timestamps only.

--- src/main/java/com/google/changestreams/sample/gcs/Main.java
+++ src/main/java/com/google/changestreams/sample/gcs/Main.java
@@ -21,6 +21,7 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.spanner.SpannerConfig;
 import org.apache.beam.sdk.io.gcp.spanner.SpannerIO;
+import org.apache.beam.sdk.io.gcp.spanner.changestreams.model.DataChangeRecord;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
@@ -67,10 +68,10 @@ public class Main {
             .withInclusiveEndAt(after10Minutes)
         )
 
-        // Maps records to strings (commit timestamp only)
+        // Maps records to strings
         .apply(MapElements
             .into(TypeDescriptors.strings())
-            .via(record -> record.getCommitTimestamp().toString())
+            .via(DataChangeRecord::toString)
         )
 
         // Group records into 1 minute windows
